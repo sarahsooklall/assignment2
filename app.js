7e2f051fb0446b98229fd1585ae1a1b13331baf1
Yanping Li <yanping.li@f1006-31-31863.bmcc.cuny.edu>
2018-03-19 14:40:13 -0400

    fixed buttons onclick  problem

--- app.js
+++ app.js
@@ -1,36 +1,151 @@
 /*
 ** Get the users button
 */
-const questionButton = [
-    document.getElementById('sweet'),
-    document.getElementById('sour'),
-    document.getElementById('spicy'),
-    document.getElementById('mild'),
-    document.getElementById('blue'),
-    document.getElementById('red'),
-    document.getElementById('grey'),
-    document.getElementById('yellow'),
-];
+
 
 /*
 ** Question 1
 */
-questionButton[0].onclick = function(){
-    const answer = document.getElementById('buttons').value;
+const questionButton1 = [
+    document.getElementById('sweet'),
+    document.getElementById('sour'),
+    document.getElementById('spicy'),
+    document.getElementById('mild'),
+];
+
+questionButton1[0].onclick = function(){
+    const answer = document.getElementById('buttons1').value;
     document.getElementById('q2').style.display = "block";
     document.getElementById('q1').style.display = "none";
 };
-
+questionButton1[1].onclick = function(){
+    const answer = document.getElementById('buttons1').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton1[2].onclick = function(){
+    const answer = document.getElementById('buttons1').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton1[3].onclick = function(){
+    const answer = document.getElementById('buttons1').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
 /*
 ** Question 2
 */
-questionButton[1].onclick = function(){
-    const answer = document.getElementById('buttons1').value;
+const questionButton2 = [
+    document.getElementById('brownRice'),
+    document.getElementById('whiteRice'),
+    document.getElementById('greenSalad'),
+    document.getElementById('Pasta'),
+]
+questionButton2[0].onclick = function(){
+    const answer = document.getElementById('buttons2').value;
     document.getElementById('q3').style.display = "block";
     document.getElementById('q2').style.display = "none";
 };
-questionButton[2].onclick = function(){
+questionButton2[1].onclick = function(){
     const answer = document.getElementById('buttons2').value;
-    document.getElementById('q4').style.display = "block";
-    document.getElementById('q3').style.display = "none";
+    document.getElementById('q3').style.display = "block";
+    document.getElementById('q2').style.display = "none";
+};
+questionButton2[2].onclick = function(){
+    const answer = document.getElementById('buttons2').value;
+    document.getElementById('q3').style.display = "block";
+    document.getElementById('q2').style.display = "none";
+};
+questionButton2[3].onclick = function(){
+    const answer = document.getElementById('buttons2').value;
+    document.getElementById('q3').style.display = "block";
+    document.getElementById('q2').style.display = "none";
+};
+/*
+** Question 3
+*/
+const questionButton3 = [
+    document.getElementById('tea'),
+    document.getElementById('coffee'),
+    document.getElementById('wine'),
+    document.getElementById('water'),
+]
+questionButton3[0].onclick = function(){
+    const answer = document.getElementById('buttons3').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton3[1].onclick = function(){
+    const answer = document.getElementById('buttons3').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton3[2].onclick = function(){
+    const answer = document.getElementById('buttons3').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton3[3].onclick = function(){
+    const answer = document.getElementById('buttons3').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+/*
+** Question 4
+*/
+const questionButton4 = [
+    document.getElementById('blue'),
+    document.getElementById('red'),
+    document.getElementById('grey'),
+    document.getElementById('yellow'),
+]
+questionButton4[0].onclick = function(){
+    const answer = document.getElementById('buttons4').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton4[1].onclick = function(){
+    const answer = document.getElementById('buttons4').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton4[2].onclick = function(){
+    const answer = document.getElementById('buttons4').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton4[3].onclick = function(){
+    const answer = document.getElementById('buttons4').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+/*
+** Question 5
+*/
+const questionButton5 = [
+    document.getElementById('blue'),
+    document.getElementById('red'),
+    document.getElementById('grey'),
+    document.getElementById('yellow'),
+]
+questionButton5[0].onclick = function(){
+    const answer = document.getElementById('buttons5').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton5[1].onclick = function(){
+    const answer = document.getElementById('buttons5').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton5[2].onclick = function(){
+    const answer = document.getElementById('buttons5').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
+};
+questionButton5[3].onclick = function(){
+    const answer = document.getElementById('buttons5').value;
+    document.getElementById('q2').style.display = "block";
+    document.getElementById('q1').style.display = "none";
 };
