4f1ef3e2e0dc77ec959a7e2db10b4515cd058b6b
= <sarahsooklall@yahoo.com>
2018-03-21 20:09:54 -0400

    Fixing onclick code for the images.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ startButton.onclick = function (){
     document.getElementById('box').style.display = "none";
 }
 
-//for score
+//For Score - To Start At Zero
 var answerContinent = {
     africa: 0,
     europe: 0,
@@ -33,44 +33,39 @@ const captions = [
 const container = document.getElementById("q1");
 for (let i = 0; i < 4; i++) {
     
-    
     const imgContainer = document.createElement('div');
     
     const img = new Image();
     img.src = images[i];
     img.id = "img"+i;
     img.classList.add("slide");
+    images[i];
     
-    window.onload = function () {
-        document.getElementById("img0").onclick = function () {
-            answerContinent.africa++;
-            console.log(answerContinent);
-            document.getElementById('q1').style.display = "none";
-            document.getElementById('q2').style.display = "block";
-        };
-
-        document.getElementById("img1").onclick = function () {
-            answerContinent.europe++;
-            console.log(answerContinent);
-            document.getElementById('q1').style.display = "none";
-            document.getElementById('q2').style.display = "block";
-        };
-
-        document.getElementById("img2").onclick = function () {
-            answerContinent.asia++;
-            console.log(answerContinent);
-            document.getElementById('q1').style.display = "none";
-            document.getElementById('q2').style.display = "block";
-        };
-
-        document.getElementById("img3").onclick = function () {
-            answerContinent.america++;
-            console.log(answerContinent);
-            document.getElementById('q1').style.display = "none";
-            document.getElementById('q2').style.display = "block";
-        };
+    images[0].onclick = function() {
+		answerContinent.africa++;
+        console.log(answerContinent.africa);
+        document.getElementById('q1').style.display = "none";
+        document.getElementById('q2').style.display = "block";
+	};
+    images[1].onclick = function() {
+        answerContinent.europe++;
+        console.log(answerContinent.europe);
+        document.getElementById('q1').style.display = "none";
+        document.getElementById('q2').style.display = "block";
     };
-    
+    images[2].onclick = function() {
+        answerContinent.asia++;
+        console.log(answerContinent.asia);
+        document.getElementById('q1').style.display = "none";
+        document.getElementById('q2').style.display = "block";
+    };
+    images[3].onclick = function() {
+        answerContinent.america++;
+        console.log(answerContinent.america);
+        document.getElementById('q1').style.display = "none";
+        document.getElementById('q2').style.display = "block";
+    };
+        
     imgContainer.appendChild(img);
     
     const caption = document.createElement('p');
@@ -105,35 +100,31 @@ for (let i = 4; i < 8; i++) {
     img.src = baseImages[i];
     img.id = "img"+i;
     img.classList.add("slide");
+    baseImages[i];
     
-    window.onload = function () {
-        document.getElementById("img4").onclick = function () {
-            answerContinent.africa++;
-            console.log(answerContinent);
-            document.getElementById('q2').style.display = "none";
-            document.getElementById('q3').style.display = "block";
-        };
-
-        document.getElementById("img5").onclick = function () {
-            answerContinent.europe++;
-            console.log(answerContinent);
-            document.getElementById('q2').style.display = "none";
-            document.getElementById('q3').style.display = "block";
-        };
-
-        document.getElementById("img6").onclick = function () {
-            answerContinent.asia++;
-            console.log(answerContinent);
-            document.getElementById('q2').style.display = "none";
-            document.getElementById('q3').style.display = "block";
-        };
-
-        document.getElementById("img7").onclick = function () {
-            answerContinent.america++;
-            console.log(answerContinent);
-            document.getElementById('q2').style.display = "none";
-            document.getElementById('q3').style.display = "block";
-        };
+    baseImages[0].onclick = function() {
+		answerContinent.africa++;
+        console.log(answerContinent.africa);
+        document.getElementById('q2').style.display = "none";
+        document.getElementById('q3').style.display = "block";
+	};
+    baseImages[1].onclick = function() {
+        answerContinent.europe++;
+        console.log(answerContinent.europe);
+        document.getElementById('q2').style.display = "none";
+        document.getElementById('q3').style.display = "block";
+    };
+    baseImages[2].onclick = function() {
+        answerContinent.asia++;
+        console.log(answerContinent.asia);
+        document.getElementById('q2').style.display = "none";
+        document.getElementById('q3').style.display = "block";
+    };
+    baseImages[3].onclick = function() {
+        answerContinent.america++;
+        console.log(answerContinent.america);
+        document.getElementById('q2').style.display = "none";
+        document.getElementById('q3').style.display = "block";
     };
     
     imgContainer.appendChild(img);
@@ -170,35 +161,31 @@ for (let i = 8; i < 12; i++) {
     img.src = beverageImages[i];
     img.id = "img"+i;
     img.classList.add("slide");
+    beverageImages[i];
     
-    window.onload = function () {
-        document.getElementById("img8").onclick = function () {
-            answerContinent.africa++;
-            console.log(answerContinent);
-            document.getElementById('q3').style.display = "none";
-            document.getElementById('q4').style.display = "block";
-        };
-
-        document.getElementById("img9").onclick = function () {
-            answerContinent.europe++;
-            console.log(answerContinent);
-            document.getElementById('q3').style.display = "none";
-            document.getElementById('q4').style.display = "block";
-        };
-
-        document.getElementById("img610").onclick = function () {
-            answerContinent.asia++;
-            console.log(answerContinent);
-            document.getElementById('q3').style.display = "none";
-            document.getElementById('q4').style.display = "block";
-        };
-
-        document.getElementById("img11").onclick = function () {
-            answerContinent.america++;
-            console.log(answerContinent);
-            document.getElementById('q3').style.display = "none";
-            document.getElementById('q4').style.display = "block";
-        };
+    beverageImages[0].onclick = function() {
+		answerContinent.africa++;
+        console.log(answerContinent.africa);
+        document.getElementById('q3').style.display = "none";
+        document.getElementById('q4').style.display = "block";
+	};
+    beverageImages[1].onclick = function() {
+        answerContinent.europe++;
+        console.log(answerContinent.europe);
+        document.getElementById('q3').style.display = "none";
+        document.getElementById('q4').style.display = "block";
+    };
+    beverageImages[2].onclick = function() {
+        answerContinent.asia++;
+        console.log(answerContinent.asia);
+        document.getElementById('q3').style.display = "none";
+        document.getElementById('q4').style.display = "block";
+    };
+    beverageImages[3].onclick = function() {
+        answerContinent.america++;
+        console.log(answerContinent.america);
+        document.getElementById('q3').style.display = "none";
+        document.getElementById('q4').style.display = "block";
     };
     
     imgContainer.appendChild(img);
@@ -235,35 +222,31 @@ for (let i = 12; i < 16; i++) {
     img.src = perferImages[i];
     img.id = "img"+i;
     img.classList.add("slide");
+    perferImages[i];
     
-    window.onload = function () {
-        document.getElementById("img12").onclick = function () {
-            answerContinent.africa++;
-            console.log(answerContinent);
-            document.getElementById('q4').style.display = "none";
-            document.getElementById('q5').style.display = "block";
-        };
-
-        document.getElementById("img13").onclick = function () {
-            answerContinent.europe++;
-            console.log(answerContinent);
-            document.getElementById('q4').style.display = "none";
-            document.getElementById('q5').style.display = "block";
-        };
-
-        document.getElementById("img14").onclick = function () {
-            answerContinent.asia++;
-            console.log(answerContinent);
-            document.getElementById('q4').style.display = "none";
-            document.getElementById('q5').style.display = "block";
-        };
-
-        document.getElementById("img15").onclick = function () {
-            answerContinent.america++;
-            console.log(answerContinent);
-            document.getElementById('q4').style.display = "none";
-            document.getElementById('q5').style.display = "block";
-        };
+    perferImages[0].onclick = function() {
+		answerContinent.africa++;
+        console.log(answerContinent.africa);
+        document.getElementById('q4').style.display = "none";
+        document.getElementById('q5').style.display = "block";
+	};
+    perferImages[1].onclick = function() {
+        answerContinent.europe++;
+        console.log(answerContinent.europe);
+        document.getElementById('q4').style.display = "none";
+        document.getElementById('q5').style.display = "block";
+    };
+    perferImages[2].onclick = function() {
+        answerContinent.asia++;
+        console.log(answerContinent.asia);
+        document.getElementById('q4').style.display = "none";
+        document.getElementById('q5').style.display = "block";
+    };
+    perferImages[3].onclick = function() {
+        answerContinent.america++;
+        console.log(answerContinent.america);
+        document.getElementById('q4').style.display = "none";
+        document.getElementById('q5').style.display = "block";
     };
     
     imgContainer.appendChild(img);
@@ -300,35 +283,31 @@ const imgContainer = document.createElement('div');
     img.src = meatImages[i];
     img.id = "img"+i;
     img.classList.add("slide");
+    meatImages[i];
     
-    window.onload = function () {
-        document.getElementById("img16").onclick = function () {
-            answerContinent.africa++;
-            console.log(answerContinent);
-            document.getElementById('q5').style.display = "none";
-            document.getElementById('q6').style.display = "block";
-        };
-
-        document.getElementById("img17").onclick = function () {
-            answerContinent.europe++;
-            console.log(answerContinent);
-            document.getElementById('q5').style.display = "none";
-            document.getElementById('q6').style.display = "block";
-        };
-
-        document.getElementById("img18").onclick = function () {
-            answerContinent.asia++;
-            console.log(answerContinent);
-            document.getElementById('q5').style.display = "none";
-            document.getElementById('q6').style.display = "block";
-        };
-
-        document.getElementById("img19").onclick = function () {
-            answerContinent.america++;
-            console.log(answerContinent);
-            document.getElementById('q5').style.display = "none";
-            document.getElementById('q6').style.display = "block";
-        };
+    meatImages[0].onclick = function() {
+		answerContinent.africa++;
+        console.log(answerContinent.africa);
+        document.getElementById('q5').style.display = "none";
+        document.getElementById('q6').style.display = "block";
+	};
+    meatImages[1].onclick = function() {
+        answerContinent.europe++;
+        console.log(answerContinent.europe);
+        document.getElementById('q5').style.display = "none";
+        document.getElementById('q6').style.display = "block";
+    };
+    meatImages[2].onclick = function() {
+        answerContinent.asia++;
+        console.log(answerContinent.asia);
+        document.getElementById('q5').style.display = "none";
+        document.getElementById('q6').style.display = "block";
+    };
+    meatImages[3].onclick = function() {
+        answerContinent.america++;
+        console.log(answerContinent.america);
+        document.getElementById('q5').style.display = "none";
+        document.getElementById('q6').style.display = "block";
     };
     
     imgContainer.appendChild(img);
@@ -359,43 +338,37 @@ const comfortCaptions = [
 const comfortContainer = document.getElementById("q6");
 for (let i = 20; i < 24; i++) {
     
-//    images[i];
-    
     const imgContainer = document.createElement('div');
     
     const img = new Image();
     img.src = comfortImages[i];
     img.id = "img"+i;
     img.classList.add("slide");
+    comfortImages[i];
     
-    window.onload = function () {
-        document.getElementById("img20").onclick = function () {
-            answerContinent.africa++;
-            console.log(answerContinent);
-            document.getElementById('q6').style.display = "none";
-            document.getElementById('endResult').style.display = "block";
-        };
-
-        document.getElementById("img21").onclick = function () {
-            answerContinent.europe++;
-            console.log(answerContinent);
-            document.getElementById('q6').style.display = "none";
-            document.getElementById('endResult').style.display = "block";
-        };
-
-        document.getElementById("img22").onclick = function () {
-            answerContinent.asia++;
-            console.log(answerContinent);
-            document.getElementById('q6').style.display = "none";
-            document.getElementById('endResult').style.display = "block";
-        };
-
-        document.getElementById("img23").onclick = function () {
-            answerContinent.america++;
-            console.log(answerContinent);
-            document.getElementById('q6').style.display = "none";
-            document.getElementById('endResult').style.display = "block";
-        };
+    comfortImages[0].onclick = function() {
+		answerContinent.africa++;
+        console.log(answerContinent.africa);
+        document.getElementById('q6').style.display = "none";
+        document.getElementById('endResult').style.display = "block";
+	};
+    comfortImages[1].onclick = function() {
+        answerContinent.europe++;
+        console.log(answerContinent.europe);
+        document.getElementById('q6').style.display = "none";
+        document.getElementById('endResult').style.display = "block";
+    };
+    comfortImages[2].onclick = function() {
+        answerContinent.asia++;
+        console.log(answerContinent.asia);
+        document.getElementById('q6').style.display = "none";
+        document.getElementById('endResult').style.display = "block";
+    };
+    comfortImages[3].onclick = function() {
+        answerContinent.america++;
+        console.log(answerContinent.america);
+        document.getElementById('q6').style.display = "none";
+        document.getElementById('endResult').style.display = "block";
     };
     
     imgContainer.appendChild(img);
@@ -411,33 +384,31 @@ for (let i = 20; i < 24; i++) {
 /*
 ** End Result
 */
-function end() {
-    
-    var answerCont = [
-    "Africa",
-    "Europe",
-    "Asia",
-    "America"
-    ];
+//function end() {
+//    
+//    var answerCont = [
+//    "Africa",
+//    "Europe",
+//    "Asia",
+//    "America"
+//    ];
     
     // Creating new element for answer
-    var answer = document.createElement("h4");
-    document.body.appendChild(answer);
-    
-    var score;
-    
-    if (answerContinent.africa < 2 ){
-        answer.textContent = answerCont[0];
-    }else if (answerContinent.europe < 2 ){
-        answer.textContent = answerCont[1];
-    }else if (answerContinent.asia < 2 ){
-        answer.textContent = answerCont[2];
-    }else if (answerContinent.america < 2 ){
-        answer.textContent = answerCont[3];
-    }else {
-        answer.textContent != answerCont;
-    }
-    
-    document.getElementById('endResult').style.display = "block";
-    
-};
+//    var answer = document.createElement("h4");
+//    document.body.appendChild(answer);
+//    
+//    var score;
+//    
+//    if (answerContinent.africa < 2 ){
+//        answer.textContent = answerCont[0];
+//    }else if (answerContinent.europe < 2 ){
+//        answer.textContent = answerCont[1];
+//    }else if (answerContinent.asia < 2 ){
+//        answer.textContent = answerCont[2];
+//    }else if (answerContinent.america < 2 ){
+//        answer.textContent = answerCont[3];
+//    }else {
+//        answer.textContent != answerCont;
+//    }
+//    
+//};
